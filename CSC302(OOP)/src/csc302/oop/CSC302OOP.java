df6e5d0f9a6519d4ee98cb4776ec09b92e4a1c65
Chimaje Agada <94375715+chimaje@users.noreply.github.com>
2024-03-19 15:55:27 +0100

    Add files via upload

---
+++ CSC302(OOP)/src/csc302/oop/CSC302OOP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package csc302.oop;
+
+/**
+ *
+ * @author EXAMS
+ */
+public class CSC302OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
